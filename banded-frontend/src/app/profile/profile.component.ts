0b937165cc1c691dc3230d5f9c5801615c077283
Poonyapat-S <poonpb@gmail.com>
2022-03-27 17:14:28 -0400

    Linked edit bio and delete profile

--- banded-frontend/src/app/profile/profile.component.ts
+++ banded-frontend/src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Profile, ProfileService } from '../service/profile/profile.service';
+import { TokenService } from '../service/auth/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -9,8 +11,12 @@ import { Profile, ProfileService } from '../service/profile/profile.service';
 })
 export class ProfileComponent implements OnInit {
   public currProfile: Profile;
+  public editBioText: string;
 
-  constructor(private profileService: ProfileService, private route: ActivatedRoute) {this.currProfile = new Profile("", "", "", "", "", ""); }
+  constructor(private profileService: ProfileService, private tokenService: TokenService, private router: Router, private route: ActivatedRoute) {
+    this.currProfile = new Profile("", "", "", "", "", "");
+    this.editBioText = "";
+  }
 
   ngOnInit() {
     this.currProfile = this.route.snapshot.data['profileget'];
@@ -22,6 +28,14 @@ export class ProfileComponent implements OnInit {
       console.log(data);
       this.currProfile = data
     });
+  }
+  public editProfile() {
+    this.profileService.editBio(this.currProfile.userName, this.editBioText).subscribe({next: response => window.location.reload(), error: err => alert("Unauthorized")});
+    // window.location.reload();
+  }
+
+  public deleteProfile() {
+    this.profileService.deleteProfile(this.currProfile.userName).subscribe({next: response => {alert("Profile Deleted"); this.tokenService.signOut(); this.router.navigate(['']);}, error: err => alert("Unauthorized")});
   }
 
 }
