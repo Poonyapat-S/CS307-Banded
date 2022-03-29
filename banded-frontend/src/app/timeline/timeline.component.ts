80a8888863157cf6f1ec410d0aea4667a9a833cc
Kat <katherinehulle@gmail.com>
2022-03-29 13:36:02 -0400

    post display up with sample posts; refresh button wip

--- banded-frontend/src/app/timeline/timeline.component.ts
+++ banded-frontend/src/app/timeline/timeline.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Profile, ProfileService } from '../service/profile/profile.service';
 
 @Component({
   selector: 'app-timeline',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.css']
 })
 export class TimelineComponent implements OnInit {
+  public currProfile: Profile;
 
-  constructor() { }
+  constructor(private profileService: ProfileService, private route: ActivatedRoute) {this.currProfile = new Profile("", "", "", "", "", ""); }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.currProfile = this.route.snapshot.data['profileget'];
+    console.log(this.currProfile)
+  }
+
+  public async loadProfile(): Promise<any> {
+    this.profileService.getProfile().subscribe((data: Profile) => {
+      console.log(data);
+      this.currProfile = data
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
   }
 
 }
