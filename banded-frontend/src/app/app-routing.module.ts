fca5861248beba04e167f2ee3a8807f044ffd838
Kat <katherinehulle@gmail.com>
2022-03-30 09:52:13 -0400

    all topic page display frontend

--- banded-frontend/src/app/app-routing.module.ts
+++ banded-frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { TimelineComponent } from './timeline/timeline.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileGetResolver } from './service/profile/profile-get';
 import { createPostComponent } from './createPost/createPost.component';
+import { TopicsComponent } from './topics/topics.component';
 
 const routes: Routes = [
   { path: '', component: TimelineComponent},
@@ -17,7 +18,10 @@ const routes: Routes = [
   { path: 'create-account', component: CreateAccountComponent},
   { path: 'profile', component: ProfileComponent, resolve: {profileget: ProfileGetResolver}},
 //Added Below
-  { path: 'createPost', component: createPostComponent}];
+  { path: 'createPost', component: createPostComponent},
+  { path: 'topics', component: TopicsComponent}
+];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
