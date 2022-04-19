eeb80cda594e74c7ff584d6fa0cd763858aa5662
ZtevenShang <70741679+ZtevenShang@users.noreply.github.com>
2022-04-18 21:14:57 -0400

    Added dm table functionality

--- MySQL_database.sql
+++ MySQL_database.sql
@@ -18,6 +18,27 @@ USE `cs307group27`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `dm`
+--
+
+DROP TABLE IF EXISTS `dm`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dm` (
+  `dmID` int NOT NULL AUTO_INCREMENT,
+  `senderID` int NOT NULL,
+  `recipientID` int NOT NULL,
+  `messageText` varchar(255) DEFAULT NULL,
+  `timeSent` datetime NOT NULL,
+  PRIMARY KEY (`dmID`),
+  KEY `FK_dm_recipientID_user_userID_idx` (`recipientID`),
+  KEY `FK_dm_senderID_user_userID` (`senderID`),
+  CONSTRAINT `FK_dm_recipientID_user_userID` FOREIGN KEY (`recipientID`) REFERENCES `user` (`userID`),
+  CONSTRAINT `FK_dm_senderID_user_userID` FOREIGN KEY (`senderID`) REFERENCES `user` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `post`
 --
 
@@ -27,12 +48,12 @@ DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
   `postID` int NOT NULL AUTO_INCREMENT,
   `userID` int NOT NULL,
-  `postTitle` varchar(255) DEFAULT "Default Title",
   `postText` varchar(255) DEFAULT NULL,
   `parentPostID` int DEFAULT NULL,
   `timePosted` datetime NOT NULL,
   `topicID` int DEFAULT NULL,
   `isAnon` bit(1) NOT NULL,
+  `postTitle` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`postID`),
   KEY `FK_post_userID_user_userID_idx` (`userID`),
   KEY `FK_post_topicID_topic_topicID_idx` (`topicID`),
@@ -40,7 +61,7 @@ CREATE TABLE `post` (
   CONSTRAINT `FK_post_parentPostID_post_postID` FOREIGN KEY (`parentPostID`) REFERENCES `post` (`postID`),
   CONSTRAINT `FK_post_topicID_topic_topicID` FOREIGN KEY (`topicID`) REFERENCES `topic` (`topicID`),
   CONSTRAINT `FK_post_userID_user_userID` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,7 +80,7 @@ CREATE TABLE `reaction` (
   KEY `FK_reaction_postID_post_postID_idx` (`postID`),
   CONSTRAINT `FK_reaction_postID_post_postID` FOREIGN KEY (`postID`) REFERENCES `post` (`postID`),
   CONSTRAINT `FK_reaction_userID_user_userID` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +114,7 @@ CREATE TABLE `topic` (
   `topicName` varchar(45) NOT NULL,
   PRIMARY KEY (`topicID`),
   UNIQUE KEY `topicName_UNIQUE` (`topicName`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,20 +145,20 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `userID` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
+  `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
   `bio` varchar(100) DEFAULT NULL,
-  `fav_band` varchar(45) DEFAULT NULL,
-  `fav_song` varchar(45) DEFAULT NULL,
+  `fav_band` varchar(100) DEFAULT NULL,
+  `fav_song` varchar(100) DEFAULT NULL,
   `enabled` bit(1) DEFAULT NULL,
   `locked` bit(1) DEFAULT NULL,
   `user_authorities` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -168,5 +189,4 @@ CREATE TABLE `userfollower` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-28 19:24:54
-
+-- Dump completed on 2022-04-18 21:12:27
