24ef3e78766f4275841bc7aa3fbdbff640e12ddc
sboynton1 <sboynton@purdue.edu>
2022-04-20 16:41:59 -0400

    edit information and block button

--- banded-frontend/src/app/profile/test.js
+++ banded-frontend/src/app/profile/test.js
@@ -1,5 +1,8 @@
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+
+    document.getElementById("infoForm").style.display = "none";
+    document.getElementById("myDeleteAcc").style.display = "none";
   }
 
 function closeForm() {
@@ -14,16 +17,24 @@ function submitForm() {
 
   function openFormDelete() {
     document.getElementById("myDeleteAcc").style.display = "block";
+
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("infoForm").style.display = "none";
   }
+
   function closeFormDelete() {
     document.getElementById("myDeleteAcc").style.display = "none";
   }
+
+  
 
   function submitDeletion(){
     /*Send back to timeline */
 
     closeFormDelete()
   }
+
+  
 
   function followControl () {
     if(document.getElementById('followButton').textContent == 'Follow/Unfollow' || document.getElementById('followButton').textContent == 'Follow') {
@@ -34,5 +45,70 @@ function submitForm() {
       window.dispatchEvent(new CustomEvent('unfollow'));
       document.getElementById('followButton').textContent = 'Follow';
       //alert('Successfully Unfollowed');
+    }
+  }
+
+
+  //Below is the code for changing Profile Info for Sprint 3
+  function openInfoForm() {
+    document.getElementById("infoForm").style.display = "block";
+
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("myDeleteAcc").style.display = "none";
+  }
+
+  function closeInfo() {
+    document.getElementById("infoForm").style.display = "none";
+  }
+
+  function submitNewEmail() {
+    let oldEmail = document.getElementById("oldE").value;
+    let newEmail = document.getElementById("newE").value;
+
+    //Insert some code here to validify old email is correct and new email is accepted
+
+
+    document.getElementById("oldE").value = null;
+    document.getElementById("newE").value = null;
+
+    //alert success or failure
+    alert("Email Succesfully Changed!");
+  }
+
+  function submitNewPassword() {
+
+
+    let oldPassword = document.getElementById("oldP").value;
+    let newPassword = document.getElementById("newP").value;
+    let newPassword2 = document.getElementById("newP2").value;
+
+    if (oldPassword == "" || newPassword == "" || newPassword2 == "") {
+      alert('Missing input!');
+      return;
+    } else if (document.getElementById("newP").value != document.getElementById("newP2").value) {
+      alert('Your new password does not match!');
+      return;
+    } else if (document.getElementById("newP").value == document.getElementById("oldP").value) {
+      alert('Your new password cannot be the same as the old!');
+      return;
+    } 
+    
+    
+
+
+    //Insert some code here to validify old password is correct and new password is accepted
+    document.getElementById("oldP").value = null;
+    document.getElementById("newP").value = null;
+    document.getElementById("newP2").value = null;
+    //alert success or failure
+    alert("Password Succesfully Changed!");
+  }
+
+  //Control for blocking/unblocking a user
+  function blockControl() {
+    if(document.getElementById("blockButton").textContent == "Block") {
+      document.getElementById("blockButton").textContent = "Unblock";
+    } else {
+      document.getElementById("blockButton").textContent = "Block";
     }
   }
